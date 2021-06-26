c104a44d16b7bb202822099d2b2762ed9e05c86d
André Gomes da Silva <andregds85@gmail.com>
2021-06-26 18:21:11 -0300

    Estrutura Básica de Thema em Wordpress

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+      <h1>Rodape</h1>
+    </div>
+  </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>Tema Basico</title>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+  <div id="wrapper">
+    <div id="header">
+      <h1>Cabeçalho</h1>
+    </div>
